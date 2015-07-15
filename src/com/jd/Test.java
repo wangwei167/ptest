fbc7445a92907ca7e2b5cc07e4770e284f742b31
wangwei167 <wangwei27@jd.com>
2015-07-15 17:23:15 +0800

    create new file
    创建新文件

---
+++ src/com/jd/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.jd;
+
+/**
+ * 
+ * 
+ * @author 王伟
+ * @email：wangwei27@jd.com
+ * createAt 2015年7月15日
+ */
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println(1111);
+	}
+
+}
