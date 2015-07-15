fe7b5beaa0f58f5453a0d99be2560efc3cc20503
wangwei167 <wangwei27@jd.com>
2015-07-15 17:41:07 +0800

    添加新文件

---
+++ src/com/jd/Test1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jd;
+
+/**
+ * 
+ * 
+ * @author 王伟
+ * @email：wangwei27@jd.com
+ * createAt 2015年7月15日
+ */
+public class Test1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(1111111);
+	}
+
+}
